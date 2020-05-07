998ce9346e0bf90cda0e7094e7a0116aee4eb2cc
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-05-06 22:13:09 -0400

    Finished Data Page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,5 +1,9 @@
 const passport = require("../config/passport.js");
 const db = require("../models");
+const path = require("path");
+const util = require('util');
+const fs = require("fs");
+const readFilePromise = util.promisify(fs.readFile);
 
 module.exports = function(app){
     
@@ -28,6 +32,24 @@ module.exports = function(app){
 
         res.end();
 
+    });
+
+    app.post("/api/data", async (req,res) => {
+        const selectedIndustry = req.body.selectedOption
+        const job_growth_raw = await readFilePromise(path.resolve(__dirname, "../data/job_growth.json"));
+        const job_growth_data= JSON.parse(job_growth_raw);
+        const job_predictions_raw = await readFilePromise(path.resolve(__dirname, "../data/job_predictions.json"));
+        const job_predictions_data= JSON.parse(job_predictions_raw);
+
+        // Find the object with Industry == req.body.selectedOption
+        for(let i = 0; i < job_growth_data.length; i++){
+            if(job_growth_data[i].Industry == selectedIndustry){
+                return res.json({
+                    job_growth_data: job_growth_data[i],
+                    job_prediction_data : job_predictions_data[i]
+                });
+            };
+        };
     });
 
 
