745fcd30f1f1df5038b3213ffde99e41cddf2306
gh0stl0nelyGit <khoinguyen19971997@gmail.com>
2020-05-08 00:16:57 -0400

    Changed Data Models

--- models/application.js
+++ models/application.js
@@ -4,35 +4,14 @@ module.exports = function(sequelize, DataTypes) {
         userName:{
             type: DataTypes.STRING
         },
+        resumePath: {
+            type: DataTypes.STRING
+        },
+        postID: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
 
     })
-
-
-    // Application has to belong to a post
-        Application.associate = function(models) {
-            Application.belongsTo(models.Post, {
-                foreignKey: {
-                    allowNull: false
-                  }
-            })
-        }
-
-
- 
-
-// ******************************************************************************
-// Not entierly sure how the resume part will work in terms of the db
-// I put some temp code to connect the it however
-// ******************************************************************************
-
-    // Application has to belong to a resume
-        // Application.associate = function(models){
-        //     Application.belongsTo(models.Resume,{
-        //         foreignKey: {
-        //             allowNull: false
-        //           }
-        //     })
-        // }
-
  return Application
 }
