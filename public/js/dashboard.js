1086c1ce51ad0daf109bc030be68df91c3ed38dd
Demi Wu <demiwu96@gmail.com>
2020-05-09 11:21:32 -0400

    add delete post btns for employer, fix the text editor

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,6 +1,27 @@
 $(document).ready(function () {
     $('select').formSelect();
     $('.modal').modal();
-    $('.dropdown-trigger').dropdown();
-    $('.collapsible').collapsible();
+
+    const toolbarOptions = [
+        ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+        [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+        [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+        [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+
+        [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+        [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+        [{ 'font': [] }],
+        [{ 'align': [] }],
+
+        ['clean']                                         // remove formatting button
+    ];
+
+    const quill = new Quill('#editor', {
+        modules: {
+            toolbar: toolbarOptions
+        },
+        theme: 'snow'
+    });
+
 });
